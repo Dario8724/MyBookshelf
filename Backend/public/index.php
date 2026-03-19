24dc5de7da078070a9958ba5203e4fc25b598df1
Gabriel Rezende <20240343@iade.pt>
2026-03-19 11:01:53 +0000

    created route api to track all the routes and updated index

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -1,9 +1,12 @@
 <?php
 
-header('Acess-Control-Allow-Origin: *');
-header('Acess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Acess-Control-Allow-Headers: Content-Type, Authorization');
-header('Content-Type: applications/json; charset=utf-8');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
@@ -11,11 +14,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 require_once __DIR__ . '/../app/core/Database.php';
+require_once __DIR__ .'/../app/core/Controller.php';
+require_once __DIR__ .'/../app/core/Router.php';
+require_once __DIR__ .'/../app/core/JWT.php';
 
-//Teste de ligação
-try {
-    Database::getInstance()->getConnection();
-    echo json_encode(['success' => true, 'message' => 'Ligação à base de dados OK!']);
-} catch (PDOException $e) {
-    echo json_encode(['success' => false, 'error' => $e->getMessage()]);
-}
+$router = new Router();
+require_once __DIR__ . '/../app/routes/api.php';
+
+$router->dispatch();
