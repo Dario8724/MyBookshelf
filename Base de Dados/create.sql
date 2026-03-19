beb87da040af34e1d58d038edd00878577ba2eef
Gabriel Rezende <20240343@iade.pt>
2026-03-19 21:39:11 +0000

    att create.sql

--- Base de Dados/create.sql
+++ Base de Dados/create.sql
@@ -9,7 +9,8 @@ CREATE TABLE user (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    profile_image VARCHAR(255)
+    profile_image VARCHAR(255),
+    bio VARCHAR(160)
 );
 
 -- =========================
