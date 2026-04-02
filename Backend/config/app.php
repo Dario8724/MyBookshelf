66181865788bf2912125e71a8f8635972e3dd116
Gabriel Rezende <20240343@iade.pt>
2026-04-02 15:58:51 +0100

    created google book api service

--- Backend/config/app.php
+++ Backend/config/app.php
@@ -6,3 +6,5 @@ define('JWT_EXPIRY', 604800); // 1 semana em segundos
 define('UPLOAD_PATH', __DIR__ . '/../public/uploads/profiles/');
 define('UPLOAD_MAX_SIZE', 2 * 1024 * 1024); // 2MB
 define('UPLOAD_ALLOWED_TYPES', ['image/jpeg', 'image/png', 'image/webp']);
+
+define('GOOGLE_BOOKS_API_KEY', 'AIzaSyDeRlUkyIsWmLEk49SB6X25Pkpi7PJAoio'); //API Google Books
