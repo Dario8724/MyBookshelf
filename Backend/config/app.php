1da7a48c68e8d7e60d1e54f483cb66725546658b
Gabriel Rezende <gabirezende13@gmail.com>
2026-04-04 11:16:48 +0100

    protect api key

--- Backend/config/app.php
+++ Backend/config/app.php
@@ -1,10 +1,15 @@
 <?php
 
-define('JWT_SECRET', 'mybookshelf_secret_2025');
-define('JWT_EXPIRY', 604800); // 1 semana em segundos
+$env = parse_ini_file(__DIR__ . '/../.env');
 
+// JWT
+define('JWT_SECRET', $env['JWT_SECRET'] ?? 'fallback_secret');
+define('JWT_EXPIRY', 604800); // 1 semana
+
+// Upload
 define('UPLOAD_PATH', __DIR__ . '/../public/uploads/profiles/');
 define('UPLOAD_MAX_SIZE', 2 * 1024 * 1024); // 2MB
 define('UPLOAD_ALLOWED_TYPES', ['image/jpeg', 'image/png', 'image/webp']);
 
-define('GOOGLE_BOOKS_API_KEY', 'AIzaSyDeRlUkyIsWmLEk49SB6X25Pkpi7PJAoio'); //API Google Books
+// Google Books API
+define('GOOGLE_BOOKS_API_KEY', $env['GOOGLE_BOOKS_API_KEY'] ?? '');
